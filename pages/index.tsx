6720610b42023b6d19114df4e62c4012712ac611
takaodaze <gin2798@gmail.com>
2021-08-27 05:29:24 +0900

    fmt

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,10 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
-  return (
-  <div>index page</div>
-  )
-}
+  return <div>index page</div>;
+};
 
-export default Home
+export default Home;

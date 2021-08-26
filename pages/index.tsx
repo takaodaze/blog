8453ec7b13b22ad7b5d6cf7ecb93678d272dcae4
takaodaze <gin2798@gmail.com>
2021-08-27 06:11:39 +0900

    use styled comopnent for test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,17 @@
+import styled from '@emotion/styled';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
-  return <div>index page</div>;
+  return <StyledDiv>index page</StyledDiv>;
 };
+
+const StyledDiv = styled.div`
+  background: green;
+  padding: 20px;
+  color: white;
+`;
 
 export default Home;
